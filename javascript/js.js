5dc8b6dc41f2c4130178f766f9410a29b493a49d
Konrad Kowalczyk <konradkowalczyk89@gmail.com>
2017-04-08 13:55:10 +0200

    pzypinanie funkcji do przycisku

--- javascript/js.js
+++ javascript/js.js
@@ -1,11 +1,20 @@
 $(function() {
 	$('#first').css({color: 'green', backgroundColor:'yellow'});
 	
-	alert($('#first').text());
+//	alert($('#first').text());
+//	
+//	alert($('#secound').html());
+//	$('#secound').html('<b>Cześć</b>');
+//	$('#secound').before('<p>przykładowy paragraf</p>');
+//	$('#secound').after('<p>przykładowy11 paragraf</p>');
+//	
 	
-	alert($('#secound').html());
-	$('#secound').html('<b>Cześć</b>');
-	$('#secound').before('<p>przykładowy paragraf</p>');
-	$('#secound').after('<p>przykładowy11 paragraf</p>');
+//	$('li').each(function(){
+//		alert($(this).text());
+//	})
+	
+	$('#przycisk').click(function() {
+		alert("ala jaja działa");
+	});
 	
 })
